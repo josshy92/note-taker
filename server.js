e865b96e92f03bfe72d2c0cbb3a6ae8905087189
Josshy <josshy92@hotmail.com>
2021-12-11 14:56:15 -0600

    added get and post requests and urls

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const path = require('path');
+const PORT = 3001;
+const fs = require('fs')
+const { v4: uuidv4 } = require('uuid')
+
+const app = express();
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+    fs.readFile('db/db.json', 'utf8', (err, data) => {
+        err ? console.log(err) : res.json(JSON.parse(data))
+    })
+})
+
+app.post('/api/notes', (req, res) => {
+    var title = req.body.title
+    var text = req.body.text
+    var newNote = {
+        title,
+        text,
+        id: uuidv4()
+    }
+    fs.readFile('db/db.json', 'utf8', (err, data) => {
+        var currentNotes = JSON.parse(data)
+        currentNotes.push(newNote)
+        fs.writeFile('db/db.json', JSON.stringify(currentNotes), (err) => {
+            err ? console.log(err) : console.log(newNote.title + " has been saved");;
+        })
+        res.sendFile(path.join(__dirname, 'public/notes.html'));
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`);
+});
